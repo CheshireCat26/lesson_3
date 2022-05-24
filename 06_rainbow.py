b711897a855cf4723614d976d2228afe4b56bdb5
Nail <NailMinnigalinWork@gmail.com>
2022-05-24 13:36:57 +0300

    Solved first todo

--- 06_rainbow.py
+++ 06_rainbow.py
@@ -6,9 +6,15 @@ import simple_draw as sd
 
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+width = 1200
+height = 600
+sd.resolution = (width, height)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+i = 0
+for color in rainbow_colors:
+    sd.line(sd.Point(50, 50 + (5 * i)), sd.Point(350, 450 + (5 * i)), color, 4)
+    i += 1
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
