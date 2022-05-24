011650cc1aea1ce9e13502489614560ce816e090
Nail <NailMinnigalinWork@gmail.com>
2022-05-24 13:10:39 +0300

    Solved first todo

--- 02_envelop.py
+++ 02_envelop.py
@@ -10,17 +10,26 @@
 # Использовать только операторы if/elif/else, можно вложенные
 
 envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+# paper_x, paper_y = 8, 9
 # проверить для
 # paper_x, paper_y = 9, 8
 # paper_x, paper_y = 6, 8
 # paper_x, paper_y = 8, 6
 # paper_x, paper_y = 3, 4
 # paper_x, paper_y = 11, 9
-# paper_x, paper_y = 9, 11
+paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+fit = False
+if envelop_x >= paper_x and envelop_y >= paper_y:
+    fit = True
+elif envelop_x >= paper_y and envelop_y >= paper_x:
+    fit = True
+
+if fit:
+    print("YES")
+else:
+    print("NO")
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
